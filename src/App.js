077646c409ff7d7ce415e9231c21e61a5352046a
Hannes <hannes@rang.de>
2020-06-14 21:37:51 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import imgJa from "./sweden_ja.png";
+import imgNej from "./sweden_nej.png";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const from = new Date(new Date().setDate(new Date().getDate() - 8));
+  const to = new Date(new Date().setDate(new Date().getDate() - 1));
+
+  const [data, setData] = useState({ covidCases: 0 });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = `https://api.covid19api.com/country/sweden/status/confirmed?from=${from.toISOString()}&to=${to.toISOString()}`;
+      const result = await axios(request);
+
+      var datapoints = result.data;
+
+      var newCases =
+        datapoints[datapoints.length - 1].Cases - datapoints[0].Cases;
+
+      const residents = 10000000;
+      const casesPer100k = newCases / (residents / 100000);
+
+      setData({ covidCases: casesPer100k });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Is it safe to go to Sweden?</p>
+        <img src={data.covidCases < 50 ? imgJa : imgNej} alt="logo" />
+        <p>{data.covidCases < 50 ? "YES" : "NO"}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.covidCases} new cases <br></br> per 100k inhabitants in the last
+          7 days
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
